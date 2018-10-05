057321d5cfad8b18bd78c41fdb847e78e130180e
Cameron Coffey <cameron3301@yahoo.com>
2018-10-04 21:26:34 -0500

    Fixed 'CharField' syntax in `Hacker` and `HackerProfile` models

--- hacker/models.py
+++ hacker/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Hacker(models.Model):
-    first_name = charField(max_length=100)
-    last_name = charField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     # ...
 
     def __str__(self):
         return self.last_name
 
 class HackerProfile(models.Model):
-    major = charField(max_length=100)
+    major = models.CharField(max_length=100)
     # ...
