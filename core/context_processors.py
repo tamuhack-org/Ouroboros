0c6238e24046e80d4bed1868ecd6695e91595741
Gabriel Britain <quontas@gmail.com>
2018-10-20 21:54:18 -0500

    Used a ModelForm to directly tie model changes to signup.

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,6 @@
 def event_context(request):
     return {
-        'event_name': 'Hackathon',
-        'description': 'Howdy Hack is a 24 hour coding marathon, from and for innovative hackers at Texas A&M University. Howdy Hackers will brainstorm, build, and break, before they’ll finally bring a product to a panel of judges for evaluation and hopefully win some cool prizes. Beyond hacking, Howdy Hack is about connecting creative and talented hackers at TAMU with tech companies they are interested in. We want to give hackers the opportunity to show off their capabilities to an audience who will listen. Howdy Hack offers a chance to meet recruiters from a diverse set of companies to ensure there’s a company for everybody, as well as workshops and social activities to make the most of the event.',
-        'lead_sentence': 'Let\'s Write some <strong>Redass</strong> Code'
+        'event_name': 'Sacky Hack',
+        'description': ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at efficitur erat. Mauris et hendrerit ante. Pellentesque pulvinar imperdiet velit eget dignissim. Integer vel ligula ex. Vestibulum aliquam, libero sit amet sagittis elementum, est lacus pretium augue, eget congue tellus orci vel neque. Integer vitae vestibulum nibh. Fusce quam eros, suscipit a gravida in, vehicula non neque. Maecenas varius tellus et tempus tincidunt. Quisque vitae feugiat nibh, a volutpat metus. Nulla tincidunt erat quam, id rhoncus sapien cursus ut. Quisque vitae eros ac eros convallis sodales. Vivamus aliquam ultrices nibh, vel pharetra velit rutrum eget. Mauris pharetra consequat neque, a tempus massa ultrices a.',
+        'lead_sentence': 'This is <strong>BIG</strong> text'
     }
