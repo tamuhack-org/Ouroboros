79ad73ed5156a0e0601a41322c77320fc3ad6d50
Cameron Coffey <cameron3301@yahoo.com>
2018-10-20 19:48:00 -0500

    added full Hacker model

--- hacker/models.py
+++ hacker/models.py
@@ -1,8 +1,33 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# `Hacker` model - Extends `AbstractUser` model:
+SHIRT_SIZE_CHOICES = (
+    ('XS', 'XS'),
+    ('S', 'S'),
+    ('M', 'M'),
+    ('L', 'L'),
+    ('XL', 'XL'),
+    ('XXL', 'XXL'),
+) 
+
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('NB', 'Non-binary'),
+    ('NA', 'Prefer not to disclose'),
+)
+
+CLASSIFICATION_CHOICES = (
+    ('U1', 'U1'),
+    ('U2', 'U2'),
+    ('U3', 'U3'),
+    ('U4', 'U4'),
+    ('U5', 'U5'),
+)
+
+GRAD_YEAR_CHOICES = [(i,i) for i in range(timezone.now().year, timezone.now().year + 6)]        # TO-DO TEST
 
 
 class Hacker(AbstractUser):
@@ -31,19 +56,58 @@ class Hacker(AbstractUser):
     def __str__(self):
         return self.last_name
 
-    # Hacker create() method
-    @classmethod
-    def create(cls, username, password, email, first_name, last_name):
-        return cls(
-            username=username,
-            password=password,
-            email=email,
-            first_name=first_name,
-            last_name=last_name
-        )
 
-
-# `HackerProfile` model
 class HackerProfile(models.Model):
-    major = models.CharField(max_length=100)
-    # ...
+    hacker = models.OneToOneField(          # TO-DO TEST
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    major = models.CharField(max_length=50)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=2)
+    classification = models.CharField(choices=CLASSIFICATION_CHOICES, max_length=2)
+    grad_year = models.IntegerField(choices=GRAD_YEAR_CHOICES)
+
+    class Meta:         # TO-DO TEST
+        abstract = True
+
+
+class Application(HackerProfile):
+    #resume = models.FileField( ... )           # TO-DO TEST
+    interests = models.TextField(max_length=200)
+    essay = models.TextField(max_length=200)
+    approved = models.NullBooleanField(blank=True)
+    date_approved = models.DateField(null=True, blank=True)         # TO-DO TEST
+    date_submitted = models.DateField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return '%s, %s - Profile' % (self.hacker.last_name, self.hacker.first_name)
+
+
+class Confirmation(models.Model):
+    shirt_size = models.CharField(          # TO-DO TEST
+        max_length=3,
+        choices=SHIRT_SIZE_CHOICES,
+    )
+    # dietary_restrictions =            # TO-DO TEST
+    travel_reimbursement_required = models.BooleanField(default=False)          # TO-DO TEST
+    date_confirmed = models.DateField(auto_now_add=True, blank=True)
+    hacker = models.OneToOneField(              # TO-DO TEST
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    team = models.ForeignKey(           # TO-DO TEST
+        'Team', 
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=True,
+    )
+
+    def __str__(self):
+        return '%s, %s - Confirmation' % (self.hacker.last_name, self.hacker.first_name)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
