22e09ed6021bfe92c2f7d5112a7f3a5da8c60bd6
Gabriel Britain <quontas@gmail.com>
2019-06-01 19:49:48 -0500

    Added buttons to toggle form visibility.

--- ouroboros/ouroboros/context_processors.py
+++ ouroboros/ouroboros/context_processors.py
@@ -1,6 +1,11 @@
 from django.conf import settings
+from django.utils import timesince
 
 
 def customization(request):
-    return {"event_name": settings.EVENT_NAME}
+    return {
+        "event_name": settings.EVENT_NAME,
+        "event_start_date": settings.EVENT_START_DATE,
+        "event_end_date": settings.EVENT_END_DATE,
+    }
 
