a893a0f4b8f0faa37db66cc325ea54a8a0f0b1de
Gabriel Britain <quontas@gmail.com>
2019-07-22 13:25:46 -0700

    Added volunteer form to admin. (#138)

--- ouroboros/api/urls.py
+++ ouroboros/api/urls.py
@@ -4,7 +4,7 @@ from api.views import obtain_email_auth_token
 
 urlpatterns = [
     path("login", obtain_email_auth_token, name="api-login"),
-    path("checkin", views.CheckinHacker.as_view(), name="checkin-hacker"),
-    path("food", views.CreateFoodEvent.as_view(), name="create-food-event"),
-    path("workshops", views.CreateWorkshopEvent.as_view(), name="create-workshop-event"),
+    path("checkin", views.CheckinHackerView.as_view(), name="checkin-hacker"),
+    path("food", views.CreateFoodEventView.as_view(), name="create-food-event"),
+    path("workshops", views.CreateWorkshopEventView.as_view(), name="create-workshop-event"),
 ]
