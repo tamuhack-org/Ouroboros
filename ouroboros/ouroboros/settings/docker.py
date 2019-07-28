561d83094ed397df35cedcc30afc695e1d261351
Gabriel Britain <quontas@gmail.com>
2019-07-28 14:49:25 -0700

    Changed logging to be a little more specific.

--- ouroboros/ouroboros/settings/docker.py
+++ ouroboros/ouroboros/settings/docker.py
@@ -1,6 +1,6 @@
 from .base import *
 import os
-from pprint import pprint
+from google.cloud import logging
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -41,17 +41,21 @@ DATABASES = {
     }
 }
 
+# StackDriver setup
+client = logging.Client()
+client.setup_logging()
+
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "handlers": {"console": {"class": "logging.StreamHandler"}},
-    "loggers": {
-        "django": {
-            "handlers": ["console"],
-            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+    "handlers": {
+        "stackdriver": {
+            "class": "google.cloud.logging.handlers.CloudLoggingHandler",
+            "client": client,
         }
     },
+    "loggers": {"": {"handlers": ["stackdriver"], "level": "INFO"}},
 }
 
 # Email Configuration Global Settings
