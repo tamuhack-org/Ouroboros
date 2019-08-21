2da872d83c6f2bb86fca0baf2c61e2fe48259290
Gabriel Britain <quontas@gmail.com>
2019-08-21 18:24:01 -0500

    Added admin page for volunteer applications.

--- ouroboros/volunteer/admin.py
+++ ouroboros/volunteer/admin.py
@@ -26,7 +26,11 @@ class ShiftAdmin(admin.ModelAdmin):
     list_display = ("start", "end")
     ordering = ("start",)
 
+class VolunteerApplicationAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone_number")
+
 
 admin.site.register(models.FoodEvent, FoodEventAdmin)
 admin.site.register(models.WorkshopEvent, WorkshopEventAdmin)
 admin.site.register(models.Shift, ShiftAdmin)
+admin.site.register(models.VolunteerApplication, VolunteerApplicationAdmin)
