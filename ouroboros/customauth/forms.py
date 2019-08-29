3f83bd4dddc2c9cf7e7fb9da7ad56f47e0193553
Gabriel Britain <quontas@gmail.com>
2019-08-28 23:20:08 -0500

    Added confirmation email link to re-send email.

--- ouroboros/customauth/forms.py
+++ ouroboros/customauth/forms.py
@@ -1,8 +1,11 @@
 from django import forms
-from django.forms import widgets
 from django.contrib.auth import forms as auth_forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+from django.forms import widgets
+
+from hacker import models as hacker_models
 
 
 class SignupForm(auth_forms.UserCreationForm):
@@ -31,6 +34,11 @@ class SignupForm(auth_forms.UserCreationForm):
 
 
 class LoginForm(auth_forms.AuthenticationForm):
+    error_messages = {
+        'invalid_login': "Please enter a correct %(username)s and password. Note that both fields may be "
+                         "case-sensitive. Make sure you've activated your account via email as well.",
+        'inactive': "This account is inactive.",
+    }
     username = forms.EmailField(
         widget=widgets.EmailInput(attrs={"placeholder": "Email"}), label=""
     )
@@ -42,12 +50,26 @@ class LoginForm(auth_forms.AuthenticationForm):
         fields = ["username", "password"]
 
 
+class ResendActivationEmailForm(forms.Form):
+    email = forms.EmailField(widget=widgets.EmailInput(attrs={"placeholder": "Email"}), label="")
+
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if not hacker_models.Hacker.objects.filter(email=data).exists():
+            raise ValidationError("No account with this email exists.")
+        hacker: hacker_models.Hacker = hacker_models.Hacker.objects.get(email=data)
+        if hacker.is_active:
+            raise ValidationError("This account has already been activated.")
+        return data
+
+
 class PlaceholderPasswordResetForm(auth_forms.PasswordResetForm):
     """
     It's the same as the parent form, just overriding the attributes to be placeholders
     instead of labels.
     """
     email = forms.EmailField(widget=widgets.EmailInput(attrs={"placeholder": "Email"}), label="")
+
 
 class PlaceholderSetPasswordForm(auth_forms.SetPasswordForm):
     """
