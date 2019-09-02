cdf327e9071e7f03e25d2c4f73ebfe7089e4973d
Cameron Coffey <cameron3301@yahoo.com>
2019-09-02 18:14:05 -0500

    Updated all models for the v2 branch

--- hiss/user/models.py
+++ hiss/user/models.py
@@ -1,7 +1,9 @@
+from django.core import mail
+from django.template.loader import render_to_string
+from django.utils import html, timezone
+
 from django.contrib.auth import models as auth_models
 from django.db import models
-
-from rsvp.models import Rsvp
 
 
 class EmailUserManager(auth_models.UserManager):
@@ -29,7 +31,7 @@ class EmailUserManager(auth_models.UserManager):
         return self._create_user(email, password, **extra_fields)
 
 
-class User(auth_models.AbstractUser):
+class User(auth_models.AbstractUser, auth_models.PermissionsMixin):
     """
     A representation of a user within the registration system. Users are uniquely identified by their email,
     and are inactive until they confirm their email.
@@ -54,17 +56,15 @@ class User(auth_models.AbstractUser):
     # Registration system-specific fields
 
     # Applying
-    # application = models.ForeignKey('Application', null=True)
+    application = models.ForeignKey("Application", null=True, on_delete=models.SET_NULL)
 
     # RSVPing
-    rsvp = models.ForeignKey(
-        Rsvp, null=True, on_delete=models.SET_NULL, related_name="user"
-    )
+    rsvp = models.ForeignKey("Rsvp", null=True, on_delete=models.SET_NULL, related_name="user")
     rsvp_deadline = models.DateTimeField(null=True, blank=True)
     declined_acceptance = models.BooleanField(default=False)
 
     # Volunteering
-    # volunteer_application = models.ForeignKey("VolunteerApplication", null=True)
+    # volunteer_application = models.ForeignKey("VolunteerApplication", null=True, on_delete=models.SET_NULL)
 
     # Day-of
     checked_in = models.BooleanField(default=False)
@@ -72,3 +72,30 @@ class User(auth_models.AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def didnt_rsvp_in_time(self):
+        return (
+            not getattr(self, "rsvp", None)
+            and getattr(self, "rsvp_deadline", None) is not None
+            and self.rsvp_deadline < timezone.now()
+    )
+
+    def email_hacker(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        mail.send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def email_html_hacker(self, template_name, context, subject):
+        """Send an HTML email to the hacker."""
+        html_msg = render_to_string(template_name, context)
+        msg = html.strip_tags(html_msg)
+        self.email_hacker(subject, msg, html_message=html_msg)
+
