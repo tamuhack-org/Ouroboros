b3f15237ccc077042aed0d0add5d423ebc032041
Gabriel Britain <quontas@gmail.com>
2019-09-10 10:08:54 -0500

    Updated model references.

--- hiss/user/models.py
+++ hiss/user/models.py
@@ -56,10 +56,10 @@ class User(auth_models.AbstractUser):
     # Registration system-specific fields
 
     # Applying
-    application = models.ForeignKey("Application", null=True, on_delete=models.SET_NULL)
+    application = models.ForeignKey("application.Application", null=True, on_delete=models.SET_NULL)
 
     # RSVPing
-    rsvp = models.ForeignKey("Rsvp", null=True, on_delete=models.SET_NULL, related_name="user")
+    rsvp = models.ForeignKey("rsvp.Rsvp", null=True, on_delete=models.SET_NULL, related_name="user")
     rsvp_deadline = models.DateTimeField(null=True, blank=True)
     declined_acceptance = models.BooleanField(default=False)
 
