188c6346b33e9f98ed38f797ad3b331331a61496
Cameron Coffey <37715360+cameron3301@users.noreply.github.com>
2019-09-29 16:18:06 -0500

    Add ability to export emails from admin server (#207)
    * Updated all models for the v2 branch
    
    * Add exporting email functionality to User and Application admin files
    
    * commit changes so i can switch branches
    
    * Added tests for export email functionality
    
    * Minor syntax error
    
    * Fix styling + formatting
    
    * More styling + formatting fixes
    
    * Renamed email export functions and updated the names in test files
    
    * Resolved import error, renamed variable
    
    * black formatting

--- hiss/user/tests/admin_tests/user.py
+++ hiss/user/tests/admin_tests/user.py
@@ -15,3 +15,16 @@ class UserAdminTestCase(test_case.SharedTestCase):
         )
         self.user.refresh_from_db()
         self.assertTrue(self.user.checked_in)
+
+    def test_export_user_emails(self):
+        self.client.force_login(self.admin)
+        change_url = reverse_lazy("admin:user_user_changelist")
+        response = self.client.post(
+            change_url,
+            {
+                "action": "export_user_emails",
+                admin.ACTION_CHECKBOX_NAME: [self.user.pk],
+            },
+            follow=True,
+        )
+        self.assertEquals(response.status_code, 200)
