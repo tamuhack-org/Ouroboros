7bed2732b20b9b7855ced8a49c388a749f70734f
Gabriel Britain <quontas@gmail.com>
2019-09-30 14:17:17 -0500

    Created RSVP views and associated tests. (#215)
    * Added UUID PK to RSVP model.
    
    * Created RSVP basic RSVP views.
    
    * Added tests for updateview.
    
    * Reformatted for Black compliance.
    
    * Updated migrations files.

--- hiss/rsvp/models.py
+++ hiss/rsvp/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.urls import reverse_lazy
 from multiselectfield import MultiSelectField
 
 DIETARY_RESTRICTIONS = (
@@ -26,6 +27,12 @@ class Rsvp(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     datetime_submitted = models.DateTimeField(auto_now_add=True)
-    dietary_restrictions = MultiSelectField(choices=DIETARY_RESTRICTIONS, max_length=2)
+    dietary_restrictions = MultiSelectField(
+        choices=DIETARY_RESTRICTIONS, null=True, blank=True
+    )
     shirt_size = models.CharField(choices=SHIRT_SIZES, max_length=3)
-    notes = models.TextField(max_length=500)
+    notes = models.TextField(max_length=500, null=True, blank=True)
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE, null=False)
+
+    def get_absolute_url(self):
+        return reverse_lazy("rsvp:update", args=[self.id])
---
+++ hiss/rsvp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from rsvp import views
+
+app_name = "rsvp"
+urlpatterns = [
+    path("", views.CreateRsvpView.as_view(), name="create"),
+    path("<uuid:pk>", views.UpdateRsvpView.as_view(), name="update"),
+]
