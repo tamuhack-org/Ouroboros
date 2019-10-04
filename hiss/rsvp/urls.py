68180dde167ec3e34d2ca6838fbb7a1072646570
Gabriel Britain <quontas@gmail.com>
2019-10-04 12:21:19 -0500

    Added RSVP decline view and tests. (#226)
    * Added RSVP decline view and tests.
    
    * Updated formatting for Black.

--- hiss/rsvp/urls.py
+++ hiss/rsvp/urls.py
@@ -6,4 +6,5 @@ app_name = "rsvp"
 urlpatterns = [
     path("", views.CreateRsvpView.as_view(), name="create"),
     path("<uuid:pk>", views.UpdateRsvpView.as_view(), name="update"),
+    path("decline", views.DeclineRsvpView.as_view(), name="decline"),
 ]
