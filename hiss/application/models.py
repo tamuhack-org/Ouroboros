d76a7292e5b83ca5b0c53653702a10f549522731
Gabriel Britain <quontas@gmail.com>
2019-10-28 22:33:52 -0500

    Updated application admin.

--- hiss/application/models.py
+++ hiss/application/models.py
@@ -302,8 +302,11 @@ class Application(models.Model):
     )
 
     # DEMOGRAPHIC INFORMATION
-    major = models.CharField("What's your major?", max_length=255)
     school = models.CharField("What school do you go to?", max_length=255)
+    major = models.CharField("What's your major?", max_length=255)
+    classification = models.CharField(
+        "What classification are you?", choices=CLASSIFICATIONS, max_length=2
+    )
     gender = models.CharField(
         "What's your gender?", choices=GENDERS, max_length=2, default=NO_ANSWER
     )
@@ -316,9 +319,7 @@ class Application(models.Model):
     race_other = models.CharField(
         "Self-describe", max_length=255, null=True, blank=True
     )
-    classification = models.CharField(
-        "What classification are you?", choices=CLASSIFICATIONS, max_length=2
-    )
+
     grad_year = models.IntegerField(
         "What is your anticipated graduation year?", choices=GRAD_YEARS
     )
