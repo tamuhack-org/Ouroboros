672cd4bbec16e2ba6ad56f0c722400e7e3ddaaed
Gabriel Britain <quontas@gmail.com>
2019-12-07 15:54:31 -0600

    Better pagination (#322)
    * Better pagination.
    
    * Increased page limits, added list filters.
    
    * Fixed formatting.

--- hiss/user/admin.py
+++ hiss/user/admin.py
@@ -21,8 +21,30 @@ def export_user_emails(_modeladmin, _request: HttpRequest, queryset: QuerySet):
     return response
 
 
+class HasAppliedFilter(admin.SimpleListFilter):
+    title = "has_applied"
+    parameter_name = "has_applied"
+
+    def lookups(self, request, model_admin):
+        return (("Yes", "Yes"), ("No", "No"))
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "Yes":
+            return queryset.filter(application__isnull=False)
+        else:
+            return queryset.filter(application__isnull=True)
+        return queryset
+
+
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("email", "is_active", "is_staff")
+    list_display = (
+        "email",
+        "is_active",
+        "is_staff",
+        "has_applied",
+    )
+    list_filter = ("is_active", "is_staff", HasAppliedFilter)
     readonly_fields = ("email", "password")
     fieldsets = [
         ("User Information", {"fields": ["email", "password"]}),
@@ -34,9 +56,13 @@ class UserAdmin(admin.ModelAdmin):
             },
         ),
     ]
+    list_per_page = 2000
 
     export_user_emails.short_description = "Export Emails of Selected Users"
     actions = [export_user_emails]
+
+    def has_applied(self, obj: User):
+        return obj.application_set.exists()
 
 
 admin.site.register(User, UserAdmin)
