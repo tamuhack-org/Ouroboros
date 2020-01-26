ee35d05aa7d22edc9a9aa1e03b3d10a3d68ba9f5
Gabriel Britain <quontas@gmail.com>
2020-01-26 09:49:11 -0600

    Added volunteer admin panel.

---
+++ hiss/volunteer/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from rangefilter.filter import DateTimeRangeFilter
+
+from volunteer.models import FoodEvent, WorkshopEvent
+
+
+class FoodEventAdmin(admin.ModelAdmin):
+    list_filter = (
+        ("timestamp", DateTimeRangeFilter),
+    )
+
+
+class WorkshopEventAdmin(admin.ModelAdmin):
+    list_filter = (
+        ("timestamp", DateTimeRangeFilter),
+    )
+
+
+admin.site.register(FoodEvent, FoodEventAdmin)
+admin.site.register(WorkshopEvent, WorkshopEventAdmin)
