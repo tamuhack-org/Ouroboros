9f748d9aa1be146949cdce8024736dfd285f0271
Gabriel Britain <quontas@gmail.com>
2020-01-26 10:23:37 -0600

    Volunteer admin (#333)
    * Added volunteer admin panel.
    
    * Fixed formatting.
    
    * Added list display

--- hiss/volunteer/admin.py
+++ hiss/volunteer/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
+from django_admin_listfilter_dropdown.filters import ChoiceDropdownFilter
 from rangefilter.filter import DateTimeRangeFilter
 
 from volunteer.models import FoodEvent, WorkshopEvent
 
 
 class FoodEventAdmin(admin.ModelAdmin):
-    list_filter = (("timestamp", DateTimeRangeFilter),)
+    list_filter = (
+        ("timestamp", DateTimeRangeFilter),
+        ("meal", ChoiceDropdownFilter),
+        ("restrictions", ChoiceDropdownFilter),
+    )
+    list_display = ("timestamp", "meal", "restrictions", "user")
 
 
 class WorkshopEventAdmin(admin.ModelAdmin):
     list_filter = (("timestamp", DateTimeRangeFilter),)
+    list_display = ("timestamp", "user")
 
 
 admin.site.register(FoodEvent, FoodEventAdmin)
