2834a70cf6e332d6ab066291ef7d738e114f2ff8
Daniel Abreo <dabreo43@gmail.com>
2020-06-04 22:05:12 -0500

    Replaced all hard-coded TAMUHack strings

--- hiss/hiss/settings/customization.py
+++ hiss/hiss/settings/customization.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 
 MAX_YEARS_ADMISSION = 5
-EVENT_NAME = "TAMUhack"
+EVENT_NAME = "TAMU Datathon"
 EVENT_START_DATETIME = timezone.datetime(2020, 1, 25, hour=9, minute=0, second=0)
 EVENT_END_DATETIME = timezone.datetime(2020, 1, 26, hour=12, minute=0, second=0)
 
