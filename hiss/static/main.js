955bbfac8307cb14a5d1d6d86c359212ed1aec0f
phulsechinmay <phulsechinmay@gmail.com>
2020-06-19 16:08:08 -0500

    Multi-select fields now have custom styling. Moved all javascript to main.js

--- hiss/static/main.js
+++ hiss/static/main.js
@@ -1,4 +1,9 @@
-$(function() {
+$(document).ready(function() {
+    const optionalQuestionsStyle = "color:#4286f3; padding: 2em 0em 1em 0em;";
+    const optionalQuestionsText = "The following questions are optional. Scroll down to submit your application, or continue to help us improve the event!"
+    const optionalQuestionsNode = `<h6 style="${optionalQuestionsStyle}">${optionalQuestionsText}</br></h6>`
+    $("#id_shirt_size").parent().append(optionalQuestionsNode);
+
     if (!$('#id_race input[value="O"]')[0].checked) {
         $('#id_race_other').parent().hide();
     }
@@ -36,4 +41,10 @@ $(function() {
              $('#id_school_other').parent().hide();
          }
     });
+
+    // Custom styling for multi-select inputs.
+    // Reference: https://select2.org/getting-started/basic-usage
+    $('#id_majors').select2();
+    $('#id_minors').select2();
+    $('#id_technology_experience').select2();
 })
