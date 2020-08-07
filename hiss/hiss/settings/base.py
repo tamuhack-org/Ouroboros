55e74fb6d390d0d6ae2c72d02c6f8f0cbd34c225
Cameron Brill <c00brill@gmail.com>
2020-08-06 21:55:17 -0500

    Added Whitenoise to allow Django to serve static files in prod on Heroku.

--- hiss/hiss/settings/base.py
+++ hiss/hiss/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
