e7d6602021af40e910ca0332496cb140587a9a62
Cameron Brill <c00brill@gmail.com>
2020-08-10 22:10:00 -0500

    Fixed formatting.

--- hiss/application/filesize_validation.py
+++ hiss/application/filesize_validation.py
@@ -1,11 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 
+
 @deconstructible
 class FileSizeValidator(object):
     """Validator to ensure files are of a desired size or less"""
+
     message = "File size %(filesize)s is too large. Files must be less than: %(max_filesize)s."
-    code = 'invalid_filesize'
+    code = "invalid_filesize"
 
     def __init__(self, max_filesize=None, message=None, code=None):
         """Note: max_filesize will be interpreted as MB (i.e. max_filesize=1.2 -> 1.2 MB)"""
@@ -23,15 +25,15 @@ class FileSizeValidator(object):
                 self.message,
                 code=self.code,
                 params={
-                    'filesize': f'{round(size / mb_to_bytes, 2)} MB',
-                    'max_filesize': f'{self.max_filesize} MB'
-                }
+                    "filesize": f"{round(size / mb_to_bytes, 2)} MB",
+                    "max_filesize": f"{self.max_filesize} MB",
+                },
             )
 
     def __eq__(self, other):
         return (
-            isinstance(other, self.__class__) and
-            self.max_filesize == other.max_filesize and
-            self.message == other.message and
-            self.code == other.code
+            isinstance(other, self.__class__)
+            and self.max_filesize == other.max_filesize
+            and self.message == other.message
+            and self.code == other.code
         )
