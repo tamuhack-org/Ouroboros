fa41c6ff1a093b431d79aaff85ca0c5e5f1904eb
s-fletcher <sfletchertx@gmail.com>
2020-10-28 23:30:46 -0500

    Add keepAwake

--- hiss/static/main.js
+++ hiss/static/main.js
@@ -48,4 +48,32 @@ $(function() {
              $('#id_school_other').parent().hide();
          }
     });
+
+    const http = require('http'); // importing http
+
+    /**
+     * Function to keep heroku app awake. Currently this pings the
+     * hacklahoma-stats.herokuapp.com domain every 20 minutes. Keep this until we
+     * find a better hosting solution, but while we have the free plan of heroku we
+     * need to keep this to keep it awake.
+     */ 
+    setInterval(() => {
+        const options = {
+            host: 'hacklahoma-register-2021.herokuapp.com',
+            port: 80,
+            path: '/',
+        };
+        http.get(options, (res) => {
+            res.on('data', () => {
+                try {
+                    // optional logging... disable after it's working
+                    console.log('Ping!');
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', (err) => {
+            console.log(`Error: ${err.message}`);
+        });
+      }, 20 * 60 * 1000); // load every 20 minutes
 })
