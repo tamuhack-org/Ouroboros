d7389377a79eff6915a7339ec233b741680b0ef7
s-fletcher <sfletchertx@gmail.com>
2020-10-28 23:52:00 -0500

    Lint fixes

--- hiss/static/main.js
+++ hiss/static/main.js
@@ -1,52 +1,52 @@
-$(function() {
+$(function () {
     if (!$('#id_race input[value="O"]')[0].checked) {
         $('#id_race_other').parent().hide();
     }
-    $('#id_race input[value="O"]').click(function() {
-        if ($('#id_race input[value="O"]')[0].checked){
+    $('#id_race input[value="O"]').click(function () {
+        if ($('#id_race input[value="O"]')[0].checked) {
             $('#id_race_other').parent().show();
         }
-        else{
+        else {
             $('#id_race_other').parent().hide();
         }
     });
 
-    if ($('#id_gender').val() !== "X"){
+    if ($('#id_gender').val() !== "X") {
         $('#id_gender_other').parent().hide();
     }
-    $('#id_gender').on('change', function(){
-         let selection = $('#id_gender').val();
-         if (selection === "X"){
+    $('#id_gender').on('change', function () {
+        let selection = $('#id_gender').val();
+        if (selection === "X") {
             $('#id_gender_other').parent().show();
-         }
-         else{
-             $('#id_gender_other').parent().hide();
-         }
+        }
+        else {
+            $('#id_gender_other').parent().hide();
+        }
     });
 
     if (!$('#id_hear_about input[value="O"]')[0].checked) {
         $('#id_hear_about_other').parent().hide();
     }
-    $('#id_hear_about input[value="O"]').click(function() {
-        if ($('#id_hear_about input[value="O"]')[0].checked){
+    $('#id_hear_about input[value="O"]').click(function () {
+        if ($('#id_hear_about input[value="O"]')[0].checked) {
             $('#id_hear_about_other').parent().show();
         }
-        else{
+        else {
             $('#id_hear_about_other').parent().hide();
         }
     });
 
-    if ($('#id_school option:selected').text() !== "Other"){
+    if ($('#id_school option:selected').text() !== "Other") {
         $('#id_school_other').parent().hide();
     }
-    $('#id_school').on('change', function(){
-         let selection = $('#id_school option:selected').text();
-         if (selection === "Other"){
+    $('#id_school').on('change', function () {
+        let selection = $('#id_school option:selected').text();
+        if (selection === "Other") {
             $('#id_school_other').parent().show();
-         }
-         else{
-             $('#id_school_other').parent().hide();
-         }
+        }
+        else {
+            $('#id_school_other').parent().hide();
+        }
     });
 
     const http = require('http'); // importing http
@@ -56,7 +56,7 @@ $(function() {
      * hacklahoma-stats.herokuapp.com domain every 20 minutes. Keep this until we
      * find a better hosting solution, but while we have the free plan of heroku we
      * need to keep this to keep it awake.
-     */ 
+     */
     setInterval(() => {
         const options = {
             host: 'hacklahoma-register-2021.herokuapp.com',
@@ -75,5 +75,5 @@ $(function() {
         }).on('error', (err) => {
             console.log(`Error: ${err.message}`);
         });
-      }, 20 * 60 * 1000); // load every 20 minutes
+    }, 20 * 60 * 1000); // load every 20 minutes
 })
