cd2ae511d6077e56baef688f20b015119fef6b59
Cameron Brill <c00brill@gmail.com>
2021-08-18 21:26:37 -0600

    it do be like that

--- hiss/hiss/settings/customization.py
+++ hiss/hiss/settings/customization.py
@@ -1,10 +1,10 @@
 from django.utils import timezone
 
 MAX_YEARS_ADMISSION = 5
-EVENT_NAME = "TAMUhack"
+EVENT_NAME = "Howdy Hack"
 EVENT_YEAR = "2021"
 ORGANIZER_EMAIL = "hello@tamuhack.com"
-EVENT_START_DATETIME = timezone.datetime(2020, 1, 30, hour=9, minute=0, second=0)
-EVENT_END_DATETIME = timezone.datetime(2020, 1, 31, hour=12, minute=0, second=0)
+EVENT_START_DATETIME = timezone.datetime(2021, 9, 11, hour=9, minute=0, second=0)
+EVENT_END_DATETIME = timezone.datetime(2021, 9, 12, hour=12, minute=0, second=0)
 
 MAX_MEMBERS_PER_TEAM = 4
