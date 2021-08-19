04f00bd6a9d53b9c17a92207b8bf4d74bfff71ab
Cameron Brill <c00brill@gmail.com>
2021-08-18 21:26:56 -0600

    forgive me for i have sinned

--- hiss/hiss/settings/customization.py
+++ hiss/hiss/settings/customization.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 
 MAX_YEARS_ADMISSION = 5
-EVENT_NAME = "Howdy Hack"
+EVENT_NAME = "HowdyHack"
 EVENT_YEAR = "2021"
 ORGANIZER_EMAIL = "hello@tamuhack.com"
 EVENT_START_DATETIME = timezone.datetime(2021, 9, 11, hour=9, minute=0, second=0)
