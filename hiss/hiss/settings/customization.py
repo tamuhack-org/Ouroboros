dd8173c69cc40e8006c2f5f0965a2f63e63aa42f
Nathan <nathan.reddy@tamu.edu>
2021-12-08 12:55:37 -0600

    Migrating HH21 -> TH22

--- hiss/hiss/settings/customization.py
+++ hiss/hiss/settings/customization.py
@@ -1,10 +1,10 @@
 from django.utils import timezone
 
 MAX_YEARS_ADMISSION = 5
-EVENT_NAME = "HowdyHack"
-EVENT_YEAR = "2021"
+EVENT_NAME = "TAMUhack"
+EVENT_YEAR = "2022"
 ORGANIZER_EMAIL = "hello@tamuhack.com"
-EVENT_START_DATETIME = timezone.datetime(2021, 9, 11, hour=9, minute=0, second=0)
-EVENT_END_DATETIME = timezone.datetime(2021, 9, 12, hour=12, minute=0, second=0)
+EVENT_START_DATETIME = timezone.datetime(2022, 1, 29, hour=9, minute=0, second=0)
+EVENT_END_DATETIME = timezone.datetime(2022, 1, 30, hour=12, minute=0, second=0)
 
 MAX_MEMBERS_PER_TEAM = 4
