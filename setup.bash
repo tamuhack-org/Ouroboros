ee2c0f10c594b121c33ed2deaf60c370fca9e1dd
Cameron <c00brill@gmail.com>
2024-05-03 15:42:40 -0400

    fix stuff

--- setup.bash
+++ setup.bash
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-mise install python
+curl https://mise.run | sh
 
-mise plugin add poetry
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+
+~/.local/bin/mise install python
+
+~/.local/bin/mise plugin add poetry
 
 # the following sed is a hack to get around poetry not following python shims by default (which are configured by mise)
 # shellcheck disable=SC2016
 sed -i '' -e 's/curl -sSL "$install_url"/curl -sSL "$install_url" | sed '\''s\/symlinks=False\/symlinks=True\/'\''/g' ~/.local/share/mise/plugins/poetry/bin/install
 
-mise install
+~/.local/bin/mise install
 
 lefthook install
