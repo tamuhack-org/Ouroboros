0523802d07f798ae67c924c65a259d2591d4a1e0
Cameron <c00brill@gmail.com>
2024-05-03 15:14:01 -0400

    make sure all requisite files are initialized before installation

--- setup.bash
+++ setup.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mise install python
+
+mise plugin add poetry
+
+# the following sed is a hack to get around poetry not following python shims by default (which are configured by mise)
 # shellcheck disable=SC2016
 sed -i '' -e 's/curl -sSL "$install_url"/curl -sSL "$install_url" | sed '\''s\/symlinks=False\/symlinks=True\/'\''/g' ~/.local/share/mise/plugins/poetry/bin/install
 
