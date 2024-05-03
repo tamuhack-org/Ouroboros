bd4690c7fb59465e22bf76f8a26efb50fbc735f1
Cameron <c00brill@gmail.com>
2024-05-03 12:46:54 -0400

    fix poetry install

---
+++ setup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016
+sed -i '' -e 's/curl -sSL "$install_url"/curl -sSL "$install_url" | sed '\''s\/symlinks=False\/symlinks=True\/'\''/g' ~/.local/share/mise/plugins/poetry/bin/install
