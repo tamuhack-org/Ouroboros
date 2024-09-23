52c1559db5e4741819024e23f9fc310789d096d3
NitroGuy10 <naviyer10@gmail.com>
2024-09-22 20:19:23 -0500

    change emails

--- hiss/application/emails.py
+++ hiss/application/emails.py
@@ -37,7 +37,7 @@ def send_confirmation_email(app: Application) -> None:
     :type app: Application
     :return: None
     """
-    subject = f"HowdyHack: Important Day-Of Information"
+    subject = f"HowdyHack: Thanks for RSVP'ing!"
     email_template = "application/emails/confirmed.html"
     context = {
         "first_name": app.first_name,
