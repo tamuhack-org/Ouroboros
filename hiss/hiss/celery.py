ab7b22e1bf6589e5fe812b5b71ac06d7501942f9
skandrigi <skandrigi@gmail.com>
2024-11-29 02:44:53 -0600

    add required SSL configuration to celery app

--- hiss/hiss/celery.py
+++ hiss/hiss/celery.py
@@ -1,12 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 import os
-
+import ssl
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hiss.settings.dev')
 
-app = Celery('hiss')
+app = Celery('hiss',
+            broker_use_ssl={
+                'ssl_cert_reqs': ssl.CERT_REQUIRED,
+            },
+            redis_backend_use_ssl = {
+                'ssl_cert_reqs': ssl.CERT_REQUIRED,
+            }
+        )
 
 # Using a string here so the worker doesn't have to serialize the object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
@@ -14,12 +21,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Celery configuration for Redis as the broker and result backend
 app.conf.broker_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')  
 app.conf.result_backend = os.environ.get('REDIS_URL', 'redis://localhost:6379/0') 
-app.conf.update(
-    broker_use_ssl={
-        'ssl_cert_reqs': 'CERT_REQUIRED',
-    },
-)
-
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
