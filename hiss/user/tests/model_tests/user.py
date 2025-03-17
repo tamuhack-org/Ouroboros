39d8ceb94d989521c0382e93c27f4205e766f86f
MDSAJ66 <ajmds66@gmail.com>
2025-03-17 01:11:43 -0500

    Added a couple more user tests

--- hiss/user/tests/model_tests/user.py
+++ hiss/user/tests/model_tests/user.py
@@ -15,3 +15,22 @@ class UserTestCase(TestCase):
     def test_user_is_inactive_by_default(self):
         User.objects.create_user(self.email, self.password)
         self.assertFalse(User.objects.get(email=self.email).is_active)
+
+    def test_null_email_rejection(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(None, self.password)
+    
+    def test_email_uniqueness(self):
+        User.objects.create_user(self.email, self.password)
+        with self.assertRaises(Exception):
+            User.objects.create_user(self.email, "different_password")
+
+    def test_user_activation(self):
+        user = User.objects.create_user(self.email, self.password)
+        self.assertFalse(user.is_active)
+        
+        user.is_active = True
+        user.save()
+        
+        user_reloaded = User.objects.get(email=self.email)
+        self.assertTrue(user_reloaded.is_active)
