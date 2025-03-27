454b7c5fa2e235cf83912a52e4204512fc2d7e45
skandrigi <skandrigi@gmail.com>
2025-03-26 23:42:56 -0500

    feat: update admin dashboard statistics and improve card styling for better clarity

--- hiss/application/utils.py
+++ hiss/application/utils.py
@@ -11,12 +11,17 @@ def dashboard_callback(request, context):
     Callbacks are used to get data in python and send it over to the django template to be queried whenever the admin dashboard loads
     '''
     context['total_confirmed'] = Application.objects.filter(status=STATUS_CONFIRMED).count()
-    context['total_admitted'] = Application.objects.filter(status=STATUS_ADMITTED).count()
+    context['total_application'] = Application.objects.count()
     context['total_checked'] = Application.objects.filter(status=STATUS_CHECKED_IN).count()
-    context['total_rejected'] = Application.objects.filter(status=STATUS_REJECTED).count()
+    context['total_admitted'] = Application.objects.filter(status=STATUS_ADMITTED).count()
 
     context['gender_male'] = Application.objects.filter(gender=MALE).count()
     context['gender_female'] = Application.objects.filter(gender=FEMALE).count()
+
+    context['male_checked_in'] = Application.objects.filter(gender=MALE,status=STATUS_CHECKED_IN).count()
+    context['female_checked_in'] = Application.objects.filter(gender=FEMALE,status=STATUS_CHECKED_IN).count()
+
+    context['total_hardware'] = Application.objects.filter(wares="H").count()
 
 
     return context
