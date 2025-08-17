b30829227186a3aeb33cb6205931c23475da9f6f
Eric Li <eric.li.8736@gmail.com>
2025-08-17 01:07:13 -0700

    Revert "add validation before saving"
    This reverts commit 20ad4da805a78d8d48d39ada7b04608e0e83a5d6.

--- hiss/application/models.py
+++ hiss/application/models.py
@@ -313,7 +313,6 @@ class Application(models.Model):
     @override
     def save(self, *args, **kwargs):
         """Override save to ensure meal group assignment logic is applied."""
-        self.full_clean()
         self.assign_meal_group()
         super().save(*args, **kwargs)
 
