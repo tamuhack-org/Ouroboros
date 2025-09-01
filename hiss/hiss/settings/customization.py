4a2015c84f1154ff84783a784ad9f0718e0d9ba1
MDSAJ66 <ajmds66@gmail.com>
2025-08-31 19:54:16 -0500

    Changed Event Titles

--- hiss/hiss/settings/customization.py
+++ hiss/hiss/settings/customization.py
@@ -1,13 +1,13 @@
 from django.utils import timezone
 
 MAX_YEARS_ADMISSION = 6
-EVENT_NAME = "TAMUhack"
+EVENT_NAME = "HowdyHack"
 EVENT_YEAR = "2025"
 ORGANIZER_NAME = "TAMUhack"
 ORGANIZER_EMAIL = "hello@tamuhack.com"
-EVENT_START_DATETIME = timezone.datetime(2025, 1, 25, hour=9, minute=0, second=0)
-EVENT_END_DATETIME = timezone.datetime(2025, 1, 26, hour=12, minute=0, second=0)
-EVENT_DATE_TEXT = "January 25-26, 2025"
+EVENT_START_DATETIME = timezone.datetime(2025, 10, 18, hour=9, minute=0, second=0)
+EVENT_END_DATETIME = timezone.datetime(2025, 10, 19, hour=12, minute=0, second=0)
+EVENT_DATE_TEXT = "October 18-19, 2025"
 
 MAX_MEMBERS_PER_TEAM = 4
 APPLE_WALLET_S3_BUCKET_URL = "https://th25-apple-wallet-passes.s3.amazonaws.com"
