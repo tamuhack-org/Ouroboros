a667eea454266025435a67f4ff20512e92db4061
Sandeep Kandrigi <skandrigi@gmail.com>
2025-11-30 20:29:33 -0600

    fix confirmation deadlines to use timezone of user

--- hiss/hiss/settings/customization.py
+++ hiss/hiss/settings/customization.py
@@ -8,6 +8,7 @@ ORGANIZER_EMAIL = "hello@tamuhack.com"
 EVENT_START_DATETIME = timezone.datetime(2025, 10, 18, hour=9, minute=0, second=0)
 EVENT_END_DATETIME = timezone.datetime(2025, 10, 19, hour=12, minute=0, second=0)
 EVENT_DATE_TEXT = "October 18-19, 2025"
+EVENT_TIMEZONE = "America/Chicago"
 
 APPLE_WALLET_S3_BUCKET_URL = "https://th25-apple-wallet-passes.s3.amazonaws.com"
 AWS_S3_BUCKET_NAME = "2025-th-resumes"
