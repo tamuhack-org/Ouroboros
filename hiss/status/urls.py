a892cde12856393d28d7bca665ad3ff5ab21dfe9
Eric Li <eric.li.8736@gmail.com>
2026-09-06 17:34:31 -0500

    Delete confirmed count views

--- hiss/status/urls.py
+++ hiss/status/urls.py
@@ -5,5 +5,4 @@ from status import views
 urlpatterns = [
     path("", views.StatusView.as_view(), name="status"),
     path("rsvp/<uuid:pk>/", views.RSVPSubmitView.as_view(), name="rsvp_submit"),
-    path("confirmed/", views.ConfirmedCountView.as_view(), name="confirmed_count"),
 ]
