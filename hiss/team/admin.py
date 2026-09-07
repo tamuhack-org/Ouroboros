4ffea15cf20de8b8030bf19db450108dd67e1918
Eric Li <eric.li.8736@gmail.com>
2026-09-06 21:27:42 -0500

    Create approval shell

--- hiss/team/admin.py
+++ hiss/team/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 
 from application.admin import ApplicationAdminInline
 
 from .models import Team
 
 
+@admin.action(description="Accept selected team")
+def approve(modeladmin, request, queryset: QuerySet[Team]):
+    apps = [team.get_members() for team in queryset]
+    print(apps)
+
+
 class TeamAdmin(admin.ModelAdmin):
     inlines = (ApplicationAdminInline,)
+    actions = [approve]
 
 
 admin.site.register(Team, TeamAdmin)
